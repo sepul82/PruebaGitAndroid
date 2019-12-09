2f4046ad57ee589f3d4004820d0ddd5dab9c1c72
sepul82 <davidlopezotero@gmail.com>
2019-12-09 19:27:20 +0100

    jj

---
+++ app/src/main/java/davidens/aprendiendo/AprendiendoUD1.java
@@ -0,0 +1,13 @@
+package davidens.aprendiendo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AprendiendoUD1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aprendiendo1);
+    }
+}
